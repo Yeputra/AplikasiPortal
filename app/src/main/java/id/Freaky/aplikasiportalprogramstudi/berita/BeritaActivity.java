223ce6d85740e376cf226c92cbeeb934c585107f
Yuda Eka Putra <yuda702@gmail.com>
2019-07-01 16:39:10 +0700

    Added NavDrawer

---
+++ app/src/main/java/id/Freaky/aplikasiportalprogramstudi/berita/BeritaActivity.java
@@ -0,0 +1,14 @@
+package id.Freaky.aplikasiportalprogramstudi.berita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import id.Freaky.aplikasiportalprogramstudi.R;
+
+public class BeritaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berita);
+    }
+}
